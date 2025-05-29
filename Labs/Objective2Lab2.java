1febcd1f193162f8bb1d44147e73c030f577eefc
Kathy Lam <kathy.lam3o3@gmail.com>
2025-05-29 13:37:59 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Kathy Lam";
+    int age = 40;
+    String state = "Colorado";
+
+    /*
+      Your solution goes here
+    */
+    
+  }
+}
+
+complete objective2lab2
