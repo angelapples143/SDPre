842836769d46b8ad5055d4bd27801240f0a0b997
Kathy Lam <kathy.lam3o3@gmail.com>
2025-05-29 10:36:07 -0600

    complete objective1lab1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -2,7 +2,9 @@ public class Objective1Lab1 {
   public static void main(String[] args) {
     /*
       System.out.printIn("Hello, Skill Distillery");
-  
+      System.out.printIn("complete objective1lab1");
+    
+
     */
   }
 }
