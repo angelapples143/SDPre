9fbfbf8fc809382d86e69c4cb9a1ff77e29e019e
Kathy Lam <kathy.lam3o3@gmail.com>
2025-05-29 14:05:59 -0600

    complete objective2lab3

--- Labs/Objective2Lab3.java
+++ Labs/Objective2Lab3.java
@@ -1,18 +1,19 @@
 public class Objective2Lab3 {
   public static void main(String[] args) {
     
-    //The month you were born;
+    // The month you were born
     String month = "July";
 
-    //The year you were born;
-    int year = "1984";
+    // The year you were born
+    int year = 1984;
 
-    //Is today a weekend or not;
-    boolean isweekend = false;
-    /*
-      Your solution goes here
-    */
+    // Is today a weekend or not
+    boolean isWeekend = false;
 
+    // Output the values
+    System.out.println("Month: " + month);
+    System.out.println("Year: " + year);
+    System.out.println("Is it the weekend? " + isWeekend);
   }
 }
 
