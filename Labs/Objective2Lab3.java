acabfb63da02ecd52422852913b01199bd939c48
Kathy Lam <kathy.lam3o3@gmail.com>
2025-05-29 13:54:16 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,19 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    
+    //The month you were born;
+    String month = "July";
+
+    //The year you were born;
+    int year = "1984";
+
+    //Is today a weekend or not;
+    boolean isweekend = false;
+    /*
+      Your solution goes here
+    */
+
+  }
+}
+
+complete objective2lab3
