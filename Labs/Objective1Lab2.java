5f2c44164ba6978b4105db4c0ce38bc4e1ef9fa3
Kathy Lam <kathy.lam3o3@gmail.com>
2025-05-29 10:49:54 -0600

    complete object1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,12 @@
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+    /*
+      System.out.printIn("----------------")
+      System.out.printIn("| Grocery List:  |");
+      System.out.printIn("| 1. Milk        |");
+      System.out.printIn("| 2. Eggs        |");
+      System.out.printIn("| 3. Bread       |");
+      System.out.printIn("----------------")
+    */
+  }
+}
