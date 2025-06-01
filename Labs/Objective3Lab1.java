4c06a414c9d3b88a41d1d1387d39a75e8a6f7a1b
Kathy Lam <kathy.lam3o3@gmail.com>
2025-05-31 21:51:08 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,27 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+
+        int num1 = 15;
+        int num2 = 6;
+        int num3 = 2;
+
+        // Calculate the sum of the three numbers
+        int sum = num1 + num2 + num3;
+
+        // Calculate the average, casting the sum to double to ensure accurate result
+        double average = (double) sum / 3;
+
+        // Print the sum and average
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+
+    }
+}
+
+complete objective3lab1
+
+
+   
+
+
+  
